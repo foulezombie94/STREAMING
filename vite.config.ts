fbe1768601a2ec3eaa2b3561cfb0ec8d8d35c7ab
Momo <69732506+foulezombie94@users.noreply.github.com>
2026-05-01 21:59:39 +0200

    Fix Vite routing for details.html page

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { resolve, dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { defineConfig } from 'vite'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        details: resolve(__dirname, 'details.html'),
+      },
+    },
+  },
+})
